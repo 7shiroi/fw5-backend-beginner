8666f5780f555cbc2bc251352f78ee7ffeb2befd
7shiroi <lucky7kelvin@gmail.com>
2022-02-12 18:31:04 +0800

    refactor: add async model

--- src/models/history.js
+++ src/models/history.js
@@ -12,12 +12,31 @@ exports.getHistories = (data, cb) => {
   });
 };
 
+exports.getHistoriesAsync = (data) => new Promise((resolve, reject) => {
+  db.query(`SELECT h.id history_id, u.email, v.name vehicle_name, h.date_start, h.date_end, h.has_returned, h.prepayment FROM histories h 
+  JOIN vehicles v ON h.id_vehicle = v.id 
+  JOIN users u ON h.id_user = u.id
+  WHERE v.name LIKE '${data.vehicleName}%'
+    AND u.email LIKE '${data.email}%'
+  LIMIT ${data.limit} OFFSET ${data.offset}`, (error, res) => {
+    if (error) reject(error);
+    resolve(res);
+  });
+});
+
 exports.getHistory = (id, cb) => {
   db.query('SELECT * FROM histories WHERE id=?', [id], (error, res) => {
     if (error) throw error;
     cb(res);
   });
 };
+
+exports.getHistoryAsync = (id) => new Promise((resolve, reject) => {
+  db.query('SELECT * FROM histories WHERE id=?', [id], (error, res) => {
+    if (error) reject(error);
+    resolve(res);
+  });
+});
 
 exports.getHistoriesCount = (data, cb) => {
   db.query(`SELECT COUNT(*) rowsCount FROM histories h
@@ -30,12 +49,30 @@ exports.getHistoriesCount = (data, cb) => {
   });
 };
 
+exports.getHistoriesCountAsync = (data) => new Promise((resolve, reject) => {
+  db.query(`SELECT COUNT(*) rowsCount FROM histories h
+  JOIN vehicles v ON h.id_vehicle = v.id 
+  JOIN users u ON h.id_user = u.id
+  WHERE v.name LIKE '${data.vehicleName}%'
+    AND u.email LIKE '${data.email}%'`, (error, res) => {
+    if (error) reject(error);
+    resolve(res);
+  });
+});
+
 exports.addHistory = (data, cb) => {
   db.query('INSERT INTO histories SET ?', data, (error, res) => {
     if (error) throw error;
     cb(res);
   });
 };
+
+exports.addHistoryAsync = (data) => new Promise((resolve, reject) => {
+  db.query('INSERT INTO histories SET ?', data, (error, res) => {
+    if (error) reject(error);
+    resolve(res);
+  });
+});
 
 exports.editHistory = (id, data, cb) => {
   db.query(
@@ -49,9 +86,28 @@ exports.editHistory = (id, data, cb) => {
   );
 };
 
+exports.editHistoryAsync = (id, data) => new Promise((resolve, reject) => {
+  db.query(
+    // eslint-disable-next-line max-len
+    'UPDATE histories SET ? WHERE id = ?',
+    [data, id],
+    (error, res) => {
+      if (error) reject(error);
+      resolve(res);
+    },
+  );
+});
+
 exports.deleteHistory = (id, cb) => {
   db.query('DELETE FROM histories WHERE id = ?', [id], (error, res) => {
     if (error) throw error;
     cb(res);
   });
 };
+
+exports.deleteHistoryAsync = (id) => new Promise((resolve, reject) => {
+  db.query('DELETE FROM histories WHERE id = ?', [id], (error, res) => {
+    if (error) reject(error);
+    resolve(res);
+  });
+});
