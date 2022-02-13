bbd2cb5cc1907854eed531db207ba8e0dfa3fa8e
7shiroi <lucky7kelvin@gmail.com>
2022-02-13 20:02:23 +0800

    fix: add route for index

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,6 @@
 const route = require('express').Router();
 const { getPopularVehicles } = require('../controllers/vehicle');
+const responseHandler = require('../helpers/responseHandler');
 
 route.use('/category', require('./category'));
 route.use('/vehicle', require('./vehicle'));
@@ -11,3 +12,5 @@ route.use('/profile', require('./profile'));
 route.get('/popular', getPopularVehicles);
 
 module.exports = route;
+
+route.get('/', (req, res) => responseHandler(res, 200, 'Backend is running well!'));
