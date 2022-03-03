a5e88e88f3caf92373c5220ea956c49bf39e4938
7shiroi <lucky7kelvin@gmail.com>
2022-03-03 19:18:07 +0800

    fix: revert cors to partial

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,13 +1,18 @@
 const auth = require('express').Router();
+const cors = require('cors');
 const { verifyUser: verifyUserHelper } = require('../helpers/auth');
 
 const {
   login, register, forgotPassword, verifyUser,
 } = require('../controllers/auth');
 
-auth.post('/login', login);
-auth.post('/register', register);
-auth.post('/forgotPassword', forgotPassword);
-auth.post('/verifyUser', verifyUserHelper, verifyUser);
+const corsOptions = {
+  origin: 'http://localhost:3000',
+};
+
+auth.post('/login', cors(corsOptions), login);
+auth.post('/register', cors(corsOptions), register);
+auth.post('/forgotPassword', cors(corsOptions), forgotPassword);
+auth.post('/verifyUser', cors(corsOptions), verifyUserHelper, verifyUser);
 
 module.exports = auth;
