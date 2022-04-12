ced4f7d137d90584dfe45fb4c8d2e4b87bfadc35
7shiroi <lucky7kelvin@gmail.com>
2022-04-12 19:04:22 +0800

    fix: use createPool for db connection

--- src/helpers/db.js
+++ src/helpers/db.js
@@ -1,56 +1,56 @@
 const mysql = require('mysql');
 
-// const {
-//   DB_HOST, DB_USER, DB_PASSWORD, DB_NAME,
-// } = process.env;
-
-// const conn = mysql.createConnection({
-//   host: DB_HOST,
-//   user: DB_USER,
-//   password: DB_PASSWORD,
-//   database: DB_NAME,
-// });
-
-// conn.connect();
-
-// module.exports = conn;
-
 const {
-  DB_HOST,
-  DB_USER,
-  DB_PASSWORD,
-  DB_NAME,
+  DB_HOST, DB_USER, DB_PASSWORD, DB_NAME,
 } = process.env;
 
-const config = {
+const conn = mysql.createPool({
   host: DB_HOST,
   user: DB_USER,
   password: DB_PASSWORD,
   database: DB_NAME,
-};
+});
 
-let connection;
+// conn.connect();
 
-const handleDisconnect = () => {
-  connection = mysql.createConnection(config);
-  connection.connect((err) => {
-    if (err) {
-      console.log('error when connecting to db:', err);
-      setTimeout(handleDisconnect, 2000);
-    }
-  });
+module.exports = conn;
 
-  connection.on('error', (err) => {
-    console.log('db error', err);
-    if (err.code === 'PROTOCOL_CONNECTION_LOST') {
-      handleDisconnect();
-    } else {
-      throw err;
-    }
-  });
-};
+// const {
+//   DB_HOST,
+//   DB_USER,
+//   DB_PASSWORD,
+//   DB_NAME,
+// } = process.env;
 
-handleDisconnect();
-console.log('db connected');
+// const config = {
+//   host: DB_HOST,
+//   user: DB_USER,
+//   password: DB_PASSWORD,
+//   database: DB_NAME,
+// };
 
-module.exports = connection;
+// let connection;
+
+// const handleDisconnect = () => {
+//   connection = mysql.createConnection(config);
+//   connection.connect((err) => {
+//     if (err) {
+//       console.log('error when connecting to db:', err);
+//       setTimeout(handleDisconnect, 2000);
+//     }
+//   });
+
+//   connection.on('error', (err) => {
+//     console.log('db error', err);
+//     if (err.code === 'PROTOCOL_CONNECTION_LOST') {
+//       handleDisconnect();
+//     } else {
+//       throw err;
+//     }
+//   });
+// };
+
+// handleDisconnect();
+// console.log('db connected');
+
+// module.exports = connection;
